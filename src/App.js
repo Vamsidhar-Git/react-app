29cabe667d4e039df35c2b5283c4307e1ee936df
Vamsidhar-Git <vamsidhar.naik@students.iiit.ac.in>
2021-09-05 17:02:51 +0530

    Added

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+   
+        </p>
+        <h2>hey how are you</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
